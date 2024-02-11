0f2ebebe6099386d9f2cef5a5d20bb8d672a755b
gptshubham <shubhamca76@gmail.com>
2024-02-12 00:04:03 +0530

    day1 completed

---
+++ Day1_Variables.py
@@ -0,0 +1,26 @@
+# # String Manipulation
+# # \n
+# print("Shubham\nKumar\nGupta")
+# # String Concatenation
+# print("Hello"+" "+"Shubham")
+
+# # input()
+# user_name = input('What is your name? : ')
+# print('Hello ' + user_name)
+# # Alternatively
+# print('Hello ' + input("What's your name?"))   # ***** Surprise!
+
+# # Variables
+# user_name = input('What is your name? : ')
+# length = len(user_name)
+# print(length)
+
+# Variables --> naming conventions
+# self-explanatory
+# no keywords
+# no beginning with numbers
+
+# # Micro Project 1 : Band Name Generator.
+# city = input("What's the name of the city you grew up in? : ")
+# pets_name = input("What's your Pet's Name? : ")
+# print("Your Band Name Could be %s %s" %(city,pets_name))
